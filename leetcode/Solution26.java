e2868f6d69c18cd7bb870f941467797d4508794f
Preslav Venkov <venkovpres@gmail.com>
2024-04-11 16:45:03 +0300

    Add leetcode problem 26

---
+++ leetcode/Solution26.java
@@ -0,0 +1,41 @@
+package leetcode;
+
+public class Solution26 {
+
+    //Solution for Leetcode problem 26: Remove Duplicates from Sorted Array
+    public int removeDuplicates1(int[] nums) {
+
+        int k = 1;
+
+        int pA = 0;
+
+        int pB = 0;
+
+        while (pB < nums.length) {
+            //check if the current value is something we have not seen before
+            if (nums[pB] != nums[pA]) {
+                pA++;
+                nums[pA] = nums[pB];
+                k++;
+            }
+            pB++;
+        }
+
+        return k;
+    }
+
+    public int removeDuplicates2(int[] nums) {
+        //starting from 1 as 0 will always be there as first element
+        int availableSlot = 1;
+
+        //second pointer traversing till it finds a number we have not seen before
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[i-1]) {
+                nums[availableSlot] = nums[i];
+                availableSlot++;
+            }
+        }
+
+        return availableSlot;
+    }
+}
